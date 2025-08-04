c9a219ab80d927c6124a039f2938c61f07340e72
Prometheus <g.prometheus69@gmail.com>
2025-08-04 17:13:51 +0700

    update table nhóm thành viên

--- resources/views/backend/user/catalogue/component/table.blade.php
+++ resources/views/backend/user/catalogue/component/table.blade.php
@@ -2,7 +2,7 @@
     <thead>
         <tr>
             <th><input type="checkbox" value="" id="checkAll" class="input-checkbox"></th>
-            <th>Ảnh</th>
+            <th>Tên nhóm thành viên</th>
             <th>Mô tả</th>
             <th class="text-center">Hành động</th>
 
@@ -13,10 +13,9 @@
             @foreach ($userCatalogues as $userCatalogue)
             <tr>
                 <td><input type="checkbox" class="input-checkbox"></td>
-                <td>...</td>
                 <td>{{ $userCatalogue->name }}</td>
                 <td>{{ $userCatalogue->description }}</td>
-                <td class="text-center"><input value="{{ $userCatalogue->status }}" type="checkbox" class="js-switch status" data-field="status" data-modelId = "{{ $userCatalogue->id }}" data-model="User" {{($userCatalogue->status == 1) ? 'checked' : ''}}/></td>
+                {{-- <td class="text-center"><input value="{{ $userCatalogue->status }}" type="checkbox" class="js-switch status" data-field="status" data-modelId = "{{ $userCatalogue->id }}" data-model="User" {{($userCatalogue->status == 1) ? 'checked' : ''}}/></td> --}}
                 <td class="text-center">
                     <a href="{{ route('user.catalogue.create', $userCatalogue->id) }}" class="btn btn-success"><i class="fa fa-edit"></i></a>
                     <a href="{{ route('user.catalogue.delete', $userCatalogue->id) }}" class="btn btn-danger"><i class="fa fa-trash"></i></a>
